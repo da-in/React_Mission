82560ac35a9193120cbc114561686c2706a5e04c
unknown <talown@naver.com>
2022-03-22 13:02:44 +0900

    create row

--- react-netflix-clone/src/App.js
+++ react-netflix-clone/src/App.js
@@ -1,12 +1,36 @@
 import './App.css';
 import Nav from './components/Nav';
 import Banner from './components/Banner';
+import Row from './components/Row';
+import requests from './api/requests';
 
 function App() {
     return (
-        <div className="App">
+        <div className="app">
             <Nav />
             <Banner />
+            <Row
+                title="NETFLIX ORIGINALS"
+                id="NO"
+                fetchURL={requests.fetchNetflixOriginals}
+                isLargeRow
+            />
+            <Row
+                title="Trending Now"
+                id="TN"
+                fetchURL={requests.fetchTrending}
+            />
+            <Row title="Top Rated" id="TR" fetchURL={requests.fetchTopRated} />
+            <Row
+                title="Action Movies"
+                id="AM"
+                fetchURL={requests.fetchActionMovies}
+            />
+            <Row
+                title="Comedy Movies"
+                id="CM"
+                fetchURL={requests.fetchComedyMovies}
+            />
         </div>
     );
 }
